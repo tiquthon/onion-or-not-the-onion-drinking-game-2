1bfaf74f5baa32394fd69acc471cb295da6641d5
tiquthon <tiquthon@gmail.com>
2023-01-17 20:31:20 +0100

    Add play sub components to client

--- client/src/routes/play/aftermath.rs
+++ client/src/routes/play/aftermath.rs
@@ -1,1 +1,18 @@
+use yew::{html, Component, Context, Html};
 
+pub struct AftermathComponent;
+
+impl Component for AftermathComponent {
+    type Message = ();
+    type Properties = ();
+
+    fn create(_ctx: &Context<Self>) -> Self {
+        Self {}
+    }
+
+    fn view(&self, _ctx: &Context<Self>) -> Html {
+        html! {
+            <div>{"it works!"}</div>
+        }
+    }
+}
--- client/src/routes/play/mod.rs
+++ client/src/routes/play/mod.rs
@@ -1,8 +1,22 @@
+// TODO: use std::cell::RefCell;
+// TODO: use std::rc::Rc;
+
 use fluent_templates::LanguageIdentifier;
 
-use onion_or_not_the_onion_drinking_game_2_shared_library::Game;
+// TODO: use futures_util::stream::SplitSink;
+// TODO: use futures_util::StreamExt;
 
-use yew::{html, Component, Context, ContextHandle, ContextProvider, Html};
+// TODO: use gloo_net::websocket::futures::WebSocket;
+use gloo_net::websocket::{Message, WebSocketError};
+
+// TODO: use wasm_bindgen_futures::spawn_local;
+
+use yew::{html, Component, Context, ContextHandle, Html};
+
+// TODO: use aftermath::AftermathComponent;
+use connecting::ConnectingComponent;
+// TODO: use game::GameComponent;
+// TODO: use lobby::LobbyComponent;
 
 pub mod aftermath;
 pub mod connecting;
@@ -12,7 +26,9 @@ pub mod lobby;
 pub struct PlayComponent {
     langid: LanguageIdentifier,
     _context_listener: ContextHandle<LanguageIdentifier>,
-    game: Game,
+
+    // TODO: messages: Vec<crate::components::messages::Message>,
+    state: PlayState,
 }
 
 impl Component for PlayComponent {
@@ -20,14 +36,39 @@ impl Component for PlayComponent {
     type Properties = ();
 
     fn create(ctx: &Context<Self>) -> Self {
+        // TODO: use anyhow::Context;
+
         let (langid, context_listener) = ctx
             .link()
             .context(ctx.link().callback(PlayComponentMsg::MessageContextUpdated))
             .expect("Missing LanguageIdentifier context.");
+
+        /* TODO: let web_socket_sink = WebSocket::open("ws://localhost:8080/ws")
+        .map(|web_socket: WebSocket| {
+            let (sink, mut stream) = web_socket.split();
+            let web_socket_message_received_callback = ctx
+                .link()
+                .callback(PlayComponentMsg::WebSocketMessageReceived);
+            let web_socket_closed_callback =
+                ctx.link().callback(|_| PlayComponentMsg::WebSocketClosed);
+            spawn_local(async move {
+                while let Some(msg) = stream.next().await {
+                    web_socket_message_received_callback.emit(msg);
+                }
+                web_socket_closed_callback.emit(());
+            });
+            Rc::new(RefCell::new(sink))
+        })
+        .context("Failed opening WebSocket connection.");*/
+
         Self {
             langid,
             _context_listener: context_listener,
-            game: Game::default(),
+
+            // TODO: messages: Vec::new(),
+            state: PlayState::Connecting {
+                // TODO: web_socket_sink
+            },
         }
     }
 
@@ -37,20 +78,38 @@ impl Component for PlayComponent {
                 self.langid = langid;
                 true
             }
+            _ => true,
         }
     }
 
     fn view(&self, _ctx: &Context<Self>) -> Html {
-        html! {
-            <main>
-                <ContextProvider<Option<Game>> context={self.game}>
-                    {"Play"}
-                </ContextProvider<Option<Game>>>
-            </main>
+        match &self.state {
+            PlayState::Connecting { .. } => {
+                html! { <ConnectingComponent /> }
+            } /* TODO: PlayState::Lobby => {
+                  html! { <LobbyComponent /> }
+              }
+              PlayState::Game => {
+                  html! { <GameComponent /> }
+              }
+              PlayState::Aftermath => {
+                  html! { <AftermathComponent /> }
+              } */
         }
     }
 }
 
 pub enum PlayComponentMsg {
     MessageContextUpdated(LanguageIdentifier),
+    WebSocketClosed,
+    WebSocketMessageReceived(Result<Message, WebSocketError>),
+}
+
+enum PlayState {
+    Connecting {
+        // TODO: web_socket_sink: anyhow::Result<Rc<RefCell<SplitSink<WebSocket, Message>>>>,
+    },
+    // TODO: Lobby,
+    // TODO: Game,
+    // TODO: Aftermath,
 }
