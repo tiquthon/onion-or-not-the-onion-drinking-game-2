cca8d7a089b82fbd0cc409169cd438684301abdd
tiquthon <tiquthon@gmail.com>
2023-01-20 08:25:28 +0100

    Extend server model and Implement more on WebSocket handler

--- server/src/model/mod.rs
+++ server/src/model/mod.rs
@@ -1,8 +1,11 @@
-use chrono::{DateTime, Utc};
 use std::collections::HashMap;
 use std::fmt::{Display, Formatter};
 
+use chrono::{DateTime, Utc};
+
 use uuid::Uuid;
+
+use onion_or_not_the_onion_drinking_game_2_shared_library::model as shared_model;
 
 const INVITE_CODE_CHARS: [char; 26] = [
     'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
@@ -13,6 +16,8 @@ const POSSIBLE_INVITE_CODE_COMBINATIONS: usize = INVITE_CODE_CHARS.len()
     * (INVITE_CODE_CHARS.len() - 1)
     * (INVITE_CODE_CHARS.len() - 2)
     * (INVITE_CODE_CHARS.len() - 3);
+
+/* SERVER */
 
 #[derive(Clone, Eq, PartialEq, Debug)]
 pub struct Server {
@@ -43,8 +48,10 @@ impl Default for Server {
     }
 }
 
+/* INVITE CODE */
+
 #[derive(Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug)]
-pub struct InviteCode(String);
+pub struct InviteCode(pub String);
 
 impl InviteCode {
     pub fn generate() -> Self {
@@ -63,35 +70,219 @@ impl Display for InviteCode {
     }
 }
 
+// Allowing clippy::from_over_into, because don't want to and can't implement From<_> for shared_model.
+#[allow(clippy::from_over_into)]
+impl Into<shared_model::game::InviteCode> for InviteCode {
+    fn into(self) -> shared_model::game::InviteCode {
+        shared_model::game::InviteCode(self.0)
+    }
+}
+
+/* GAME */
+
 #[derive(Clone, Eq, PartialEq, Debug)]
 pub struct Game {
+    pub configuration: GameConfiguration,
     pub game_state: GameState,
     pub players: Vec<Player>,
 }
+
+impl Game {
+    pub fn into_shared_model_game<F>(
+        self,
+        invite_code: InviteCode,
+        this_player_id: PlayerId,
+        f: F,
+    ) -> shared_model::game::Game
+    where
+        F: Fn(&QuestionId) -> RedditSubmissionData,
+    {
+        shared_model::game::Game {
+            invite_code: invite_code.into(),
+            game_state: self
+                .game_state
+                .into_shared_model_game_state(&this_player_id, f),
+            players: self.players.into_iter().map(Into::into).collect(),
+            this_player_id: this_player_id.into(),
+        }
+    }
+}
+
+/* GAME CONFIGURATION */
+
+#[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug)]
+pub struct GameConfiguration {
+    pub count_of_questions: Option<u64>,
+    pub minimum_score_per_question: Option<i64>,
+    pub maximum_answer_time_per_question: Option<u64>,
+}
+
+/* GAME STATE */
 
 #[derive(Clone, Eq, PartialEq, Debug)]
 pub enum GameState {
     InLobby,
     Playing {
-        previous_questions: Vec<QuestionId>,
+        previous_questions: Vec<(QuestionId, HashMap<PlayerId, Answer>)>,
         current_question: QuestionId,
         playing_state: PlayingState,
     },
     Aftermath {
-        questions: Vec<QuestionId>,
+        questions: Vec<(QuestionId, HashMap<PlayerId, Answer>)>,
         restart_request: Vec<PlayerId>,
     },
 }
 
+impl GameState {
+    pub fn into_shared_model_game_state<F>(
+        self,
+        own_id: &PlayerId,
+        f: F,
+    ) -> shared_model::game::GameState
+    where
+        F: Fn(&QuestionId) -> RedditSubmissionData,
+    {
+        match self {
+            GameState::InLobby => shared_model::game::GameState::InLobby,
+            GameState::Playing {
+                current_question,
+                playing_state,
+                ..
+            } => shared_model::game::GameState::Playing {
+                current_question: f(&current_question).into(),
+                playing_state: playing_state.into_shared_model_playing_state(own_id),
+            },
+            GameState::Aftermath {
+                questions,
+                restart_request,
+            } => shared_model::game::GameState::Aftermath {
+                questions: questions
+                    .into_iter()
+                    .map(|(question_id, player_answer_map)| {
+                        (
+                            f(&question_id).into(),
+                            player_answer_map
+                                .into_iter()
+                                .map(|(player_id, answer)| (player_id.into(), answer.into()))
+                                .collect(),
+                        )
+                    })
+                    .collect(),
+                restart_requests: restart_request.into_iter().map(Into::into).collect(),
+            },
+        }
+    }
+}
+
+/* QUESTION ID */
+
+#[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug)]
+pub struct QuestionId(pub Uuid);
+
+impl QuestionId {
+    pub fn generate() -> Self {
+        Self(Uuid::new_v4())
+    }
+}
+
+impl Display for QuestionId {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
+/* PLAYING STATE */
+
+#[derive(Clone, Eq, PartialEq, Debug)]
+pub enum PlayingState {
+    Question {
+        time_until: DateTime<Utc>,
+        answers: HashMap<PlayerId, Answer>,
+    },
+    Solution {
+        time_until: DateTime<Utc>,
+        answers: HashMap<PlayerId, Answer>,
+        skip_request: Vec<PlayerId>,
+    },
+}
+
+impl PlayingState {
+    pub fn into_shared_model_playing_state(
+        self,
+        own_id: &PlayerId,
+    ) -> shared_model::game::PlayingState {
+        match self {
+            PlayingState::Question {
+                time_until,
+                answers,
+            } => {
+                let own_answer = answers.get(own_id).copied();
+                shared_model::game::PlayingState::Question {
+                    time_until,
+                    answers: answers.into_keys().map(Into::into).collect(),
+                    own_answer: own_answer.map(Into::into),
+                }
+            }
+            PlayingState::Solution {
+                time_until,
+                answers,
+                skip_request,
+            } => shared_model::game::PlayingState::Solution {
+                time_until,
+                answers: answers
+                    .into_iter()
+                    .map(|(id, answer)| (id.into(), answer.into()))
+                    .collect(),
+                skip_request: skip_request.into_iter().map(Into::into).collect(),
+            },
+        }
+    }
+}
+
+/* ANSWER */
+
+#[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug)]
+pub enum Answer {
+    TheOnion,
+    NotTheOnion,
+}
+
+// Allowing clippy::from_over_into, because don't want to and can't implement From<_> for shared_model.
+#[allow(clippy::from_over_into)]
+impl Into<shared_model::game::Answer> for Answer {
+    fn into(self) -> shared_model::game::Answer {
+        match self {
+            Answer::TheOnion => shared_model::game::Answer::TheOnion,
+            Answer::NotTheOnion => shared_model::game::Answer::NotTheOnion,
+        }
+    }
+}
+
+/* PLAYER */
+
 #[derive(Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug)]
 pub struct Player {
     pub id: PlayerId,
-    pub name: String,
-    pub points: u16,
+    pub name: PlayerName,
+    pub play_type: PlayType,
 }
 
+// Allowing clippy::from_over_into, because don't want to and can't implement From<_> for shared_model.
+#[allow(clippy::from_over_into)]
+impl Into<shared_model::game::Player> for Player {
+    fn into(self) -> shared_model::game::Player {
+        shared_model::game::Player {
+            id: self.id.into(),
+            name: self.name.into(),
+            play_type: self.play_type.into(),
+        }
+    }
+}
+
+/* PLAYER ID */
+
 #[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug)]
-pub struct PlayerId(Uuid);
+pub struct PlayerId(pub Uuid);
 
 impl PlayerId {
     pub fn generate() -> Self {
@@ -105,45 +296,64 @@ impl Display for PlayerId {
     }
 }
 
-#[derive(Clone, Eq, PartialEq, Debug)]
-pub enum PlayingState {
-    Question {
-        time_until: DateTime<Utc>,
-        answers: HashMap<PlayerId, Answer>,
-    },
-    Answer {
-        time_until: DateTime<Utc>,
-        answers: HashMap<PlayerId, Answer>,
-        skip_request: Vec<PlayerId>,
-    },
-}
-
-#[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug)]
-pub enum Answer {
-    TheOnion,
-    NotTheOnion,
-}
-
-#[derive(Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug)]
-pub struct Question {
-    id: QuestionId,
-    internal: RedditSubmissionData,
-}
-
-#[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug)]
-pub struct QuestionId(Uuid);
-
-impl QuestionId {
-    pub fn generate() -> Self {
-        Self(Uuid::new_v4())
+// Allowing clippy::from_over_into, because don't want to and can't implement From<_> for shared_model.
+#[allow(clippy::from_over_into)]
+impl Into<shared_model::game::PlayerId> for PlayerId {
+    fn into(self) -> shared_model::game::PlayerId {
+        shared_model::game::PlayerId(self.0)
     }
 }
 
-impl Display for QuestionId {
+/* PLAYER NAME */
+
+#[derive(Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug)]
+pub struct PlayerName(pub String);
+
+impl Display for PlayerName {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         write!(f, "{}", self.0)
     }
 }
+
+impl TryFrom<String> for PlayerName {
+    type Error = ();
+
+    fn try_from(value: String) -> Result<Self, Self::Error> {
+        if value.trim().is_empty() {
+            panic!()
+        }
+        Ok(Self(value.trim().to_string()))
+    }
+}
+
+// Allowing clippy::from_over_into, because don't want to and can't implement From<_> for shared_model.
+#[allow(clippy::from_over_into)]
+impl Into<shared_model::game::PlayerName> for PlayerName {
+    fn into(self) -> shared_model::game::PlayerName {
+        shared_model::game::PlayerName(self.0)
+    }
+}
+
+/* PLAY TYPE */
+
+#[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug)]
+pub enum PlayType {
+    Player { points: u16 },
+    Watcher,
+}
+
+// Allowing clippy::from_over_into, because don't want to and can't implement From<_> for shared_model.
+#[allow(clippy::from_over_into)]
+impl Into<shared_model::game::PlayType> for PlayType {
+    fn into(self) -> shared_model::game::PlayType {
+        match self {
+            PlayType::Player { points } => shared_model::game::PlayType::Player { points },
+            PlayType::Watcher => shared_model::game::PlayType::Watcher,
+        }
+    }
+}
+
+/* REDDIT SUBMISSION DATA */
 
 #[derive(Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug, serde::Deserialize)]
 pub struct RedditSubmissionData {
@@ -161,4 +371,16 @@ pub struct RedditSubmissionData {
     over_18: bool,
     thumbnail: String,
     preview_image_url: Option<String>,
+}
+
+// Allowing clippy::from_over_into, because don't want to and can't implement From<_> for shared_model.
+#[allow(clippy::from_over_into)]
+impl Into<shared_model::game::Question> for RedditSubmissionData {
+    fn into(self) -> shared_model::game::Question {
+        shared_model::game::Question {
+            url: self.url,
+            title: self.title,
+            preview_image_url: self.preview_image_url,
+        }
+    }
 }
--- server/src/routes/websocket.rs
+++ server/src/routes/websocket.rs
@@ -1,8 +1,14 @@
-use actix_web::{web, Error, HttpRequest, HttpResponse};
-use actix_ws::Message;
 use std::sync::Arc;
 
+use actix_web::{web, Error, HttpRequest, HttpResponse};
+
+use actix_ws::Message;
+
 use futures_util::StreamExt;
+
+use onion_or_not_the_onion_drinking_game_2_shared_library::model::network::{
+    ClientMessage, ServerMessage,
+};
 
 #[tracing::instrument(name = "Websocket", skip(req, body))]
 pub async fn ws(
@@ -12,11 +18,46 @@ pub async fn ws(
 ) -> Result<HttpResponse, Error> {
     let (response, mut session, mut msg_stream) = actix_ws::handle(&req, body)?;
 
+    let server: Arc<tokio::sync::Mutex<crate::model::Server>> = Arc::clone(&server);
+
     actix_web::rt::spawn(async move {
+        let mut connection_store = ConnectionStore::new();
+
         while let Some(Ok(msg)) = msg_stream.next().await {
             match msg {
                 Message::Binary(bytes) => {
-                    println!("Got binary, {bytes:?} ({})", bytes.len());
+                    match ClientMessage::try_from(&*bytes) {
+                        Ok(client_message) => {
+                            if let Some(server_message) =
+                                handle_message(client_message, &server, &mut connection_store).await
+                            {
+                                let binary_server_message_result: Result<Vec<u8>, _> =
+                                    server_message.try_into();
+                                match binary_server_message_result {
+                                    Ok(binary_server_message) => {
+                                        if let Err(error) =
+                                            session.binary(binary_server_message).await
+                                        {
+                                            // TODO: HANDLE CLOSED ERROR
+                                            tracing::error!(
+                                                "It seems like client closed connection ({error})."
+                                            );
+                                        }
+                                    }
+                                    Err(error) => {
+                                        // TODO: HANDLE ERROR
+                                        tracing::error!(
+                                            "Failed converting ServerMessage ({error})."
+                                        );
+                                    }
+                                }
+                            }
+                        }
+                        Err(error) => {
+                            // TODO: HANDLE ERROR
+                            tracing::error!("Failed parsing ClientMessage ({error}).");
+                        }
+                    }
                 }
                 Message::Close(optional_close_reason) => {
                     tracing::info!(
@@ -46,4 +87,81 @@ pub async fn ws(
     });
 
     Ok(response)
+}
+
+async fn handle_message(
+    message: ClientMessage,
+    server: &Arc<tokio::sync::Mutex<crate::model::Server>>,
+    connection_store: &mut ConnectionStore,
+) -> Option<ServerMessage> {
+    match message {
+        ClientMessage::CreateLobby {
+            player_name,
+            just_watch,
+            count_of_questions,
+            minimum_score_per_question,
+            maximum_answer_time_per_question,
+        } => {
+            let new_invite_code = crate::model::InviteCode::generate();
+
+            let new_game = crate::model::Game {
+                configuration: crate::model::GameConfiguration {
+                    count_of_questions,
+                    minimum_score_per_question,
+                    maximum_answer_time_per_question,
+                },
+                game_state: crate::model::GameState::InLobby,
+                players: vec![crate::model::Player {
+                    id: connection_store.connected_player_id,
+                    name: player_name.try_into().unwrap(),
+                    play_type: match just_watch {
+                        true => crate::model::PlayType::Watcher,
+                        false => crate::model::PlayType::Player { points: 0 },
+                    },
+                }],
+            };
+
+            connection_store.in_lobby = Some(new_invite_code.clone());
+
+            let mut locked_server = server.lock().await;
+
+            locked_server
+                .games
+                .insert(new_invite_code.clone(), new_game.clone());
+
+            drop(locked_server);
+
+            Some(ServerMessage::LobbyCreated(
+                new_game.into_shared_model_game(
+                    new_invite_code,
+                    connection_store.connected_player_id,
+                    |_question_id| todo!(),
+                ),
+            ))
+        }
+        ClientMessage::JoinLobby { .. } => {
+            tracing::info!("JOIN LOBBY");
+            None
+        }
+    }
+}
+
+struct ConnectionStore {
+    connected_player_id: crate::model::PlayerId,
+    in_lobby: Option<crate::model::InviteCode>,
+}
+
+impl ConnectionStore {
+    pub fn new() -> Self {
+        Self {
+            connected_player_id: crate::model::PlayerId::generate(),
+            in_lobby: None,
+        }
+    }
+}
+
+impl Default for ConnectionStore {
+    fn default() -> Self {
+        Self::new()
+    }
 }
