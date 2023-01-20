66ea977da8a2f211c46de47da9687319af90f1aa
tiquthon <tiquthon@gmail.com>
2023-01-20 07:53:19 +0100

    Extend shared library api model

--- shared-library/src/model/game.rs
+++ shared-library/src/model/game.rs
@@ -1,28 +1,128 @@
-#[derive(Copy, Clone, Debug, Eq, PartialEq, serde::Serialize, serde::Deserialize)]
+use std::collections::HashMap;
+use std::fmt::{Display, Formatter};
+
+use chrono::{DateTime, Utc};
+
+use uuid::Uuid;
+
+/* GAME */
+
+#[derive(Clone, Eq, PartialEq, Debug, serde::Serialize, serde::Deserialize)]
 pub struct Game {
-    // TODO: pub invite_code: String,
+    pub invite_code: InviteCode,
     pub game_state: GameState,
-    // TODO: pub players: Vec<Player>,
+    pub players: Vec<Player>,
+    pub this_player_id: PlayerId,
 }
 
-impl Default for Game {
-    fn default() -> Self {
-        Game {
-            game_state: GameState::None,
-        }
+/* INVITE CODE */
+
+#[derive(
+    Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug, serde::Serialize, serde::Deserialize,
+)]
+pub struct InviteCode(pub String);
+
+impl Display for InviteCode {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)
     }
 }
 
-#[derive(Copy, Clone, Debug, Eq, PartialEq, serde::Serialize, serde::Deserialize)]
+/* GAME STATE */
+
+#[derive(Clone, Eq, PartialEq, Debug, serde::Serialize, serde::Deserialize)]
 pub enum GameState {
     InLobby,
-    Playing,
-    Aftermath,
-    None,
+    Playing {
+        current_question: Question,
+        playing_state: PlayingState,
+    },
+    Aftermath {
+        questions: Vec<(Question, HashMap<PlayerId, Answer>)>,
+        restart_requests: Vec<PlayerId>,
+    },
 }
 
+/* QUESTION */
+
+#[derive(
+    Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug, serde::Serialize, serde::Deserialize,
+)]
+pub struct Question {
+    pub url: String,
+    pub title: String,
+    pub preview_image_url: Option<String>,
+}
+
+/* PLAYING STATE */
+
+#[derive(Clone, Eq, PartialEq, Debug, serde::Serialize, serde::Deserialize)] // Default, Display
+pub enum PlayingState {
+    Question {
+        time_until: DateTime<Utc>,
+        answers: Vec<PlayerId>,
+        own_answer: Option<Answer>,
+    },
+    Solution {
+        time_until: DateTime<Utc>,
+        answers: HashMap<PlayerId, Answer>,
+        skip_request: Vec<PlayerId>,
+    },
+}
+
+/* ANSWER */
+
+#[derive(
+    Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug, serde::Serialize, serde::Deserialize,
+)]
+pub enum Answer {
+    TheOnion,
+    NotTheOnion,
+}
+
+/* PLAYER */
+
+#[derive(
+    Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug, serde::Serialize, serde::Deserialize,
+)]
 pub struct Player {
-    // TODO: pub uuid: Uuid,
-    pub name: String,
-    pub points: u16,
+    pub id: PlayerId,
+    pub name: PlayerName,
+    pub play_type: PlayType,
+}
+
+/* PLAYER ID */
+
+#[derive(
+    Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug, serde::Serialize, serde::Deserialize,
+)]
+pub struct PlayerId(pub Uuid);
+
+impl Display for PlayerId {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
+/* PLAYER NAME */
+
+#[derive(
+    Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug, serde::Serialize, serde::Deserialize,
+)]
+pub struct PlayerName(pub String);
+
+impl Display for PlayerName {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
+/* PLAY TYPE */
+
+#[derive(
+    Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug, serde::Serialize, serde::Deserialize,
+)]
+pub enum PlayType {
+    Player { points: u16 },
+    Watcher,
 }
--- shared-library/src/model/network.rs
+++ shared-library/src/model/network.rs
@@ -1,12 +1,13 @@
 use crate::model::game::Game;
 
-#[derive(serde::Serialize, serde::Deserialize)]
+#[derive(Clone, Eq, PartialEq, Debug, serde::Serialize, serde::Deserialize)]
 pub enum ClientMessage {
     CreateLobby {
         player_name: String,
+        just_watch: bool,
         count_of_questions: Option<u64>,
-        minimum_score_of_questions: Option<u64>,
-        timer: Option<u64>,
+        minimum_score_per_question: Option<i64>,
+        maximum_answer_time_per_question: Option<u64>,
     },
     JoinLobby {
         player_name: String,
@@ -15,9 +16,89 @@ pub enum ClientMessage {
     },
 }
 
-#[derive(serde::Serialize, serde::Deserialize)]
+impl TryFrom<&[u8]> for ClientMessage {
+    type Error = ClientMessageTryFromByteSliceError;
+
+    fn try_from(value: &[u8]) -> Result<Self, Self::Error> {
+        Ok(bincode::deserialize(value)?)
+    }
+}
+
+impl TryInto<Vec<u8>> for ClientMessage {
+    type Error = ClientMessageTryIntoByteVecError;
+
+    fn try_into(self) -> Result<Vec<u8>, Self::Error> {
+        Ok(bincode::serialize(&self)?)
+    }
+}
+
+#[derive(thiserror::Error, Debug)]
+pub enum ClientMessageTryFromByteSliceError {
+    #[error("Failed deserializing ClientMessage ({0})")]
+    Deserialize(bincode::Error),
+}
+
+impl From<bincode::Error> for ClientMessageTryFromByteSliceError {
+    fn from(value: bincode::Error) -> Self {
+        Self::Deserialize(value)
+    }
+}
+
+#[derive(thiserror::Error, Debug)]
+pub enum ClientMessageTryIntoByteVecError {
+    #[error("Failed serializing ClientMessage ({0})")]
+    Serialize(bincode::Error),
+}
+
+impl From<bincode::Error> for ClientMessageTryIntoByteVecError {
+    fn from(value: bincode::Error) -> Self {
+        Self::Serialize(value)
+    }
+}
+
+#[derive(Clone, Eq, PartialEq, Debug, serde::Serialize, serde::Deserialize)]
 pub enum ServerMessage {
-    LobbyCreated,
-    LobbyJoined,
+    LobbyCreated(Game),
+    LobbyJoined(Game),
     GameFullUpdate(Game),
+}
+
+impl TryFrom<&[u8]> for ServerMessage {
+    type Error = ServerMessageTryFromByteSliceError;
+
+    fn try_from(value: &[u8]) -> Result<Self, Self::Error> {
+        Ok(bincode::deserialize(value)?)
+    }
+}
+
+impl TryInto<Vec<u8>> for ServerMessage {
+    type Error = ServerMessageTryIntoByteVecError;
+
+    fn try_into(self) -> Result<Vec<u8>, Self::Error> {
+        Ok(bincode::serialize(&self)?)
+    }
+}
+
+#[derive(thiserror::Error, Debug)]
+pub enum ServerMessageTryFromByteSliceError {
+    #[error("Failed deserializing ServerMessage ({0})")]
+    Deserialize(bincode::Error),
+}
+
+impl From<bincode::Error> for ServerMessageTryFromByteSliceError {
+    fn from(value: bincode::Error) -> Self {
+        Self::Deserialize(value)
+    }
+}
+
+#[derive(thiserror::Error, Debug)]
+pub enum ServerMessageTryIntoByteVecError {
+    #[error("Failed serializing ServerMessage ({0})")]
+    Serialize(bincode::Error),
+}
+
+impl From<bincode::Error> for ServerMessageTryIntoByteVecError {
+    fn from(value: bincode::Error) -> Self {
+        Self::Serialize(value)
+    }
 }
