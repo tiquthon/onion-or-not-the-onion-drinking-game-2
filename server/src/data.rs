0eb0fe90ae1072989413476a257ef6680a0d8705
tiquthon <tiquthon@gmail.com>
2023-01-20 11:09:41 +0100

    Add dataset accessible to server code

---
+++ server/src/data.rs
@@ -0,0 +1,34 @@
+use std::collections::HashMap;
+
+use crate::model::{QuestionId, RedditSubmissionData};
+
+const RAW_NOT_THE_ONION_BEST: &str = include_str!("../assets/nottheonion.best.max2000.ron");
+const RAW_NOT_THE_ONION_TOP: &str = include_str!("../assets/nottheonion.top.max2000.ron");
+const RAW_THE_ONION_BEST: &str = include_str!("../assets/theonion.best.max2000.ron");
+const RAW_THE_ONION_TOP: &str = include_str!("../assets/theonion.top.max2000.ron");
+
+lazy_static::lazy_static! {
+    static ref NOT_THE_ONION_BEST: HashMap<QuestionId, RedditSubmissionData> = {
+        parse(RAW_NOT_THE_ONION_BEST)
+    };
+
+    static ref NOT_THE_ONION_TOP: HashMap<QuestionId, RedditSubmissionData> = {
+        parse(RAW_NOT_THE_ONION_TOP)
+    };
+
+    static ref THE_ONION_BEST: HashMap<QuestionId, RedditSubmissionData> = {
+        parse(RAW_THE_ONION_BEST)
+    };
+
+    static ref THE_ONION_TOP: HashMap<QuestionId, RedditSubmissionData> = {
+        parse(RAW_THE_ONION_TOP)
+    };
+}
+
+fn parse(data: &str) -> HashMap<QuestionId, RedditSubmissionData> {
+    ron::de::from_str::<Vec<RedditSubmissionData>>(data)
+        .unwrap()
+        .into_iter()
+        .map(|submission_data| (QuestionId::generate(), submission_data))
+        .collect()
+}
