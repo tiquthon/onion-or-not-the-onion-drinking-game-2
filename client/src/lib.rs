3c8f733c91d487a913dd7c5f294674599e28659f
tiquthon <tiquthon@gmail.com>
2023-03-05 12:37:03 +0100

    Rewrite AppComponent in client to function_component

--- client/src/lib.rs
+++ client/src/lib.rs
@@ -2,7 +2,9 @@ use fluent_templates::LanguageIdentifier;
 
 use unic_langid::langid;
 
-use yew::{html, Component, Context, ContextProvider, Html};
+use yew::{
+    function_component, html, use_effect_with_deps, use_state_eq, Callback, ContextProvider, Html,
+};
 
 use crate::components::footer::FooterComponent;
 use crate::components::header::HeaderComponent;
@@ -10,97 +12,78 @@ use crate::components::locale::{
     load_or_else_browser_select_language_identifier_and_log_warnings,
     store_language_identifier_to_persistent_storage_and_log_warnings,
 };
-
 use crate::routes::index::IndexComponent;
-use crate::routes::play::CreateJoinLobby;
-use crate::routes::play::PlayComponent;
+use crate::routes::play::{CreateJoinLobby, PlayComponent};
 
 pub mod components;
 pub mod routes;
 pub mod utils;
 
-pub struct AppComponent {
-    langid: LanguageIdentifier,
-    state: AppState,
-}
+#[function_component(AppComponent)]
+pub fn app_component() -> Html {
+    let langid = use_state_eq::<LanguageIdentifier, _>(|| langid!("en-US"));
+    let state = use_state_eq(|| AppState::Index);
 
-impl Component for AppComponent {
-    type Message = AppComponentMsg;
-    type Properties = ();
-
-    fn create(_ctx: &Context<Self>) -> Self {
-        Self {
-            langid: load_or_else_browser_select_language_identifier_and_log_warnings()
-                .unwrap_or(langid!("en-US")),
-            state: AppState::Index,
-        }
-    }
-
-    fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
-        match msg {
-            AppComponentMsg::ChangeLanguageIdentifier(lid) => {
-                store_language_identifier_to_persistent_storage_and_log_warnings(&lid);
-                self.langid = lid;
-                true
+    let cloned_langid = langid.clone();
+    use_effect_with_deps(
+        move |_| {
+            let optional_loaded_langid =
+                load_or_else_browser_select_language_identifier_and_log_warnings();
+            if let Some(loaded_langid) = optional_loaded_langid {
+                cloned_langid.set(loaded_langid);
             }
-            AppComponentMsg::JoinLobby(join_lobby) => {
-                self.state = AppState::Play {
+        },
+        (),
+    );
+
+    let inner_components = match &*state {
+        AppState::Index => {
+            let cloned_state = state.clone();
+            let on_join_lobby = Callback::from(move |join_lobby| {
+                cloned_state.set(AppState::Play {
                     create_join_lobby: CreateJoinLobby::Join(join_lobby),
-                };
-                true
-            }
-            AppComponentMsg::CreateLobby(create_lobby) => {
-                self.state = AppState::Play {
+                });
+            });
+
+            let cloned_state = state.clone();
+            let on_create_lobby = Callback::from(move |create_lobby| {
+                cloned_state.set(AppState::Play {
                     create_join_lobby: CreateJoinLobby::Create(create_lobby),
-                };
-                true
-            }
-            AppComponentMsg::NavigateToIndex => {
-                self.state = AppState::Index;
-                true
+                });
+            });
+
+            html! {
+                <IndexComponent {on_join_lobby} {on_create_lobby} />
             }
         }
-    }
+        AppState::Play { create_join_lobby } => {
+            let cloned_state = state.clone();
+            let on_go_back_to_index = Callback::from(move |_| cloned_state.set(AppState::Index));
 
-    fn view(&self, ctx: &Context<Self>) -> Html {
-        let on_change_language_identifier = ctx
-            .link()
-            .callback(AppComponentMsg::ChangeLanguageIdentifier);
-        html! {
-            <ContextProvider<LanguageIdentifier> context={self.langid.clone()}>
-                <HeaderComponent/>
-                {
-                    match &self.state {
-                        AppState::Index => {
-                            let on_join_lobby = ctx.link().callback(AppComponentMsg::JoinLobby);
-                            let on_create_lobby = ctx.link().callback(AppComponentMsg::CreateLobby);
-                            html! {
-                                <IndexComponent {on_join_lobby} {on_create_lobby} />
-                            }
-                        },
-                        AppState::Play { create_join_lobby } => {
-                            let on_go_back_to_index = ctx.link().callback(|_| AppComponentMsg::NavigateToIndex);
-                            html! {
-                                <PlayComponent create_join_lobby={create_join_lobby.clone()} {on_go_back_to_index} />
-                            }
-                        },
-                    }
-                }
-                <FooterComponent {on_change_language_identifier}/>
+            html! {
+                <PlayComponent create_join_lobby={create_join_lobby.clone()} {on_go_back_to_index} />
+            }
+        }
+    };
+
+    let cloned_langid = langid.clone();
+    let on_change_language_identifier = Callback::from(move |lid: LanguageIdentifier| {
+        store_language_identifier_to_persistent_storage_and_log_warnings(&lid);
+        cloned_langid.set(lid);
+    });
+
+    html! {
+        <>
+            <ContextProvider<LanguageIdentifier> context={(*langid).clone()}>
+                <HeaderComponent />
+                { inner_components }
+                <FooterComponent {on_change_language_identifier} />
             </ContextProvider<LanguageIdentifier>>
-        }
+        </>
     }
 }
 
-pub enum AppComponentMsg {
-    ChangeLanguageIdentifier(LanguageIdentifier),
-
-    JoinLobby(routes::index::JoinLobby),
-    CreateLobby(routes::index::CreateLobby),
-
-    NavigateToIndex,
-}
-
+#[derive(PartialEq)]
 enum AppState {
     Index,
     Play { create_join_lobby: CreateJoinLobby },
