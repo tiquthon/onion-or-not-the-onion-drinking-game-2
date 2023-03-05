c07d60fc1a70e7c0f6db8e735c8f091ae98b2a1e
tiquthon <tiquthon@gmail.com>
2023-03-05 13:41:12 +0100

    Refactor HeaderComponent in client into function_component

--- client/src/components/header.rs
+++ client/src/components/header.rs
@@ -1,51 +1,17 @@
 use fluent_templates::LanguageIdentifier;
 
-use yew::{classes, html, Component, Context, ContextHandle, Html};
+use yew::{classes, function_component, html, use_context, Html};
 
-use super::locale::LocaleComponent;
+use crate::components::locale::LocaleComponent;
 
-pub struct HeaderComponent {
-    langid: LanguageIdentifier,
-    _context_listener: ContextHandle<LanguageIdentifier>,
-}
+#[function_component(HeaderComponent)]
+pub fn header_component() -> Html {
+    let _langid = use_context::<LanguageIdentifier>().expect("Missing LanguageIdentifier context.");
 
-impl Component for HeaderComponent {
-    type Message = HeaderComponentMsg;
-    type Properties = ();
-
-    fn create(ctx: &Context<Self>) -> Self {
-        let (langid, context_listener) = ctx
-            .link()
-            .context(
-                ctx.link()
-                    .callback(HeaderComponentMsg::MessageContextUpdated),
-            )
-            .expect("Missing LanguageIdentifier context.");
-        Self {
-            langid,
-            _context_listener: context_listener,
-        }
+    html! {
+        <header class={classes!("header")}>
+            <h1 class={classes!("header__title")}><LocaleComponent keyid="game-title"/></h1>
+            <h2 class={classes!("header__subtitle")}><LocaleComponent keyid="game-subtitle"/></h2>
+        </header>
     }
-
-    fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
-        match msg {
-            HeaderComponentMsg::MessageContextUpdated(langid) => {
-                self.langid = langid;
-                true
-            }
-        }
-    }
-
-    fn view(&self, _ctx: &Context<Self>) -> Html {
-        html! {
-            <header>
-                <div class={classes!("header-headline")}><LocaleComponent keyid="game-title"/></div>
-                <div class={classes!("header-sub-headline")}><LocaleComponent keyid="game-subtitle"/></div>
-            </header>
-        }
-    }
-}
-
-pub enum HeaderComponentMsg {
-    MessageContextUpdated(LanguageIdentifier),
 }
