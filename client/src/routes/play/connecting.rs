09bef0c42876a8c0cba0217dabd0e02422a2c267
tiquthon <tiquthon@gmail.com>
2023-03-07 20:32:44 +0100

    Refactor ConnectingComponent in client into function_component

--- client/src/routes/play/connecting.rs
+++ client/src/routes/play/connecting.rs
@@ -1,82 +1,49 @@
-use yew::{classes, html, Callback, Component, Context, Html};
+use yew::{classes, function_component, html, Callback, Html};
 
 use crate::components::locale::LocaleComponent;
 
-pub struct ConnectingComponent;
-
-impl Component for ConnectingComponent {
-    type Message = ConnectingComponentMsg;
-    type Properties = ConnectingComponentProps;
-
-    fn create(_ctx: &Context<Self>) -> Self {
-        Self {}
-    }
-
-    fn update(&mut self, ctx: &Context<Self>, msg: Self::Message) -> bool {
-        match msg {
-            ConnectingComponentMsg::GoBack => {
-                ctx.props().on_go_back.emit(());
-                false
+#[function_component(ConnectingComponent)]
+pub fn connecting_component(props: &ConnectingComponentProps) -> Html {
+    match &props.state {
+        ConnectingComponentState::Connecting => {
+            let cloned_on_cancel = props.on_cancel.clone();
+            let cancel_button_onclick = Callback::from(move |_| cloned_on_cancel.emit(()));
+            html! {
+                <main class={classes!("main", "connecting-view")}>
+                    <p class={classes!("connecting-view__main-text")}>
+                        <LocaleComponent keyid="connecting-view-connecting-string" />
+                    </p>
+                    <button class={classes!("button", "connecting-view__button")}
+                        onclick={cancel_button_onclick} type="button">
+                        <LocaleComponent keyid="cancel-button-text" />
+                    </button>
+                </main>
             }
-            ConnectingComponentMsg::Cancel => {
-                ctx.props().on_cancel.emit(());
-                false
+        }
+        ConnectingComponentState::Failed {
+            locale_key_id,
+            error,
+        } => {
+            let cloned_on_go_back = props.on_go_back.clone();
+            let go_back_button_onclick = Callback::from(move |_| cloned_on_go_back.emit(()));
+            html! {
+                <main class={classes!("main", "connecting-view")}>
+                    <p class={classes!("connecting-view__main-text")}>
+                        <LocaleComponent keyid={locale_key_id.clone()} />
+                    </p>
+                    if let Some(error) = error.clone() {
+                        <p class={classes!("connecting-view__sub-text")}>
+                            {"("}{error}{")"}
+                        </p>
+                    }
+                    <button class={classes!("button", "connecting-view__button")}
+                        onclick={go_back_button_onclick} type="button">
+                        <LocaleComponent keyid="go-back-button-text" />
+                    </button>
+                </main>
             }
         }
     }
-
-    fn view(&self, ctx: &Context<Self>) -> Html {
-        match &ctx.props().state {
-            ConnectingComponentState::Connecting => {
-                let cancel_button_onclick = ctx.link().callback(|_| ConnectingComponentMsg::Cancel);
-                html! {
-                    <main class={classes!("connecting-main")}>
-                        <span class={classes!("connecting-text")}>
-                            <LocaleComponent keyid="connecting-view-connecting-string" />
-                        </span>
-                        <button type="button" onclick={cancel_button_onclick}>
-                            <LocaleComponent keyid="cancel-button-text" />
-                        </button>
-                    </main>
-                }
-            }
-            ConnectingComponentState::Failed {
-                locale_key_id,
-                error,
-            } => {
-                let go_back_button_onclick =
-                    ctx.link().callback(|_| ConnectingComponentMsg::GoBack);
-                html! {
-                    <main class={classes!("connecting-main")}>
-                        <span class={classes!("connecting-text")}>
-                            <LocaleComponent keyid={locale_key_id.clone()} />
-                        </span>
-                        <span class={classes!("connecting-sub-text")}>
-                            {
-                                if let Some(error) = error.clone() {
-                                    html! {
-                                        <>
-                                            {"("}{error}{")"}
-                                        </>
-                                    }
-                                } else {
-                                    html! {}
-                                }
-                            }
-                        </span>
-                        <button type="button" onclick={go_back_button_onclick}>
-                            <LocaleComponent keyid="go-back-button-text" />
-                        </button>
-                    </main>
-                }
-            }
-        }
-    }
-}
-
-pub enum ConnectingComponentMsg {
-    GoBack,
-    Cancel,
 }
 
 #[derive(yew::Properties, PartialEq)]
